235a282a292fec8c2ab2fe1664d207c102bcd6d2
ks009232@gmail.com <ks009232@gmail.com>
2025-03-30 09:31:02 +0530

    Storing Hashed Password

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using QuizDishtv.Data;
 using QuizDishtv.Models;
+using System.Security.Claims;
+using System.Security.Cryptography;
 
 namespace QuizDishtv.Controllers
 {
@@ -11,15 +17,109 @@ namespace QuizDishtv.Controllers
         {
             _context = context;
         }
-        public IActionResult Register(User user)
+        private string HashPassword(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return $"{Convert.ToBase64String(salt)}.{hashed}";
+        }
+
+        private bool VerifyPassword(string enteredPassword, string storedPassword)
+        {
+            var parts = storedPassword.Split('.');
+            if (parts.Length != 2)
+            {
+                return false;
+            }
+
+            var salt = Convert.FromBase64String(parts[0]);
+            var storedHash = parts[1];
+
+            string enteredHash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: enteredPassword,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return storedHash == enteredHash;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        } 
+        
+
+        [HttpPost]
+        public async Task<IActionResult> Register(User u)
         {
             if (ModelState.IsValid)
             {
-                var AddingUser = _context.Users.Add(user);
-                _context.SaveChanges();
-                return View(AddingUser);
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserEmail == u.UserEmail);
+                if (user != null)
+                {
+                    ModelState.AddModelError("UserEmail", "Email is already registered");
+                    return View(u);
+                }
+
+                u.UserPassword = HashPassword(u.UserPassword);
+                _context.Users.Add(u);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Login");
             }
-            return View(user);
+            return View(u);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string UserEmail, string UserPassword)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserEmail == UserEmail);
+                if (user != null && VerifyPassword(UserPassword, user.UserPassword))
+                {
+                    var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "Name"),
+                    new Claim(ClaimTypes.Email, "Email"),
+                    new Claim("UserId", user.UserId.ToString())
+
+                };
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var authProperties = new AuthenticationProperties();
+
+                    await HttpContext.SignInAsync(
+                       CookieAuthenticationDefaults.AuthenticationScheme,
+                       new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                    return RedirectToAction("Index", "Quiz");
+
+                }
+
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
         }
     }
 }
